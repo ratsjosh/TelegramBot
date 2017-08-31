62ef2f4a2a24b04a041345ac2597d8060b3bada0
ratsjosh <ratsjosh@outlook.com>
2017-08-31 18:17:09 +0800

    Change start command in package.json

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 var app = function(app) {
-	var TelegramBot = require('node-telegram-bot-api'),
-	    // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
-	    telegram = new TelegramBot("340965091:AAHNKChTKxpwOZXBgkoCO99gS6Z2DH_6QyU", { polling: true });
+    var TelegramBot = require('node-telegram-bot-api'),
+        // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
+        telegram = new TelegramBot("340965091:AAHNKChTKxpwOZXBgkoCO99gS6Z2DH_6QyU", {
+            polling: true
+        });
 
-	telegram.on("text", (message) => {
-	  if(message.text.toLowerCase().indexOf("/hellobot") === 0){
-      	telegram.sendMessage(message.chat.id, "Hello " + message.chat.first_name);
-	  }
-	  else {
-      	telegram.sendMessage(message.chat.id, "Hello world");
-	  }
-	});
+    telegram.on("text", (message) => {
+        let msg = message.text.toLowerCase();
+        if (msg.indexOf("/hellobot") === 0) {
+            telegram.sendMessage(message.chat.id, "Hello " + message.chat.first_name + ". How many I help you?");
+        }
+    });
 }
+
 
 module.exports = app;
